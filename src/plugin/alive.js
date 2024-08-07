c3ad49d29317311a912beaf7e38b3718ecc02eb1
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2024-08-07 12:00:12 +0500

    Update alive.js

--- src/plugin/alive.js
+++ src/plugin/alive.js
@@ -12,7 +12,7 @@ const alive = async (m, Matrix) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).toLowerCase() : '';
     if (['alive', 'uptime', 'runtime'].includes(cmd)) {
 
-  const uptimeMessage = `*🤖 ETHIX-MD Status Overview*
+  const uptimeMessage = `*🤖 KHARAL-MD Status Overview*
 _________________________________________
 
 *📆 ${days} Day*
@@ -51,7 +51,7 @@ _________________________________________
             text: uptimeMessage
           }),
           footer: proto.Message.InteractiveMessage.Footer.create({
-            text: "© Powered By 𝞢𝙏𝞖𝞘𝞦-𝞛𝘿"
+            text: "© Powered By 𝗞𝗛𝗔𝗥𝗔𝗟-𝗠𝗗"
           }),
           header: proto.Message.InteractiveMessage.Header.create({
             title: "",
@@ -68,7 +68,7 @@ _________________________________________
                   isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                   newsletterJid: '120363249960769123@newsletter',
-                  newsletterName: "Ethix-MD",
+                  newsletterName: "KHARAL-MD",
                   serverMessageId: 143
                 }
               }
